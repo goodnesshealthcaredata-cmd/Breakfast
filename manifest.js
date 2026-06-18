c966417f898f6ee903f24972346d9fe061b5c22a
goodnesshealthcaredata-cmd <goodnesshealthcaredata@gmail.com>
2026-06-18 22:40:30 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "Kanika's Snacks",
+  "short_name": "Kanika's Snacks",
+  "start_url": "/Breakfast/",
+  "scope": "/Breakfast/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "/Breakfast/icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/Breakfast/icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
